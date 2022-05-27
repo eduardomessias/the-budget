7ae92c24787e8f6c84159e04efa9eb185bde94f2
eduardomessias <eduardo.messias@outlook.com>
2022-05-27 11:34:17 +0100

    fixing default export

--- pages/api/goal/[id].js
+++ pages/api/goal/[id].js
@@ -2,17 +2,17 @@ import withDatabase from '../../../middleware/withDatabase'
 import Goal from '../../../models/goal'
 
 
-export default async function handler (req,res) {
+export async function handler(req, res) {
     try {
-        console.log (`Request query: ${JSON.stringify(req.query)}`)
-        const {id} = req.query
-        const collection = await Goal.find ({_id:id})
-        res.status (200).json ({sucess:true, data:collection})
+        console.log(`Request query: ${JSON.stringify(req.query)}`)
+        const { id } = req.query
+        const collection = await Goal.find({ _id: id })
+        res.status(200).json({ sucess: true, data: collection })
     }
     catch (e) {
-        res.status (400).json({success:false})
+        res.status(400).json({ success: false })
     }
 }
 
 
-export default withDatabase (handler)
+export default withDatabase(handler)
